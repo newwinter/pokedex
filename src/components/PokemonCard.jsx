730c88aa44d6867085bd1de3184df53a929f9638
FLORIANE BERTHIER <floriane.berthier96@gmail.com>
2023-03-28 11:21:17 +0200

    first component Pokemon card

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,8 @@
+function PokemonCard() {
+  return <figure>
+    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="bilbasaur" />
+    <figcaption>balbasaur</figcaption>
+  </figure>
+}
+
+export default PokemonCard;
