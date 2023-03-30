5b472c306ede7950baf7b145e11eff77866c1ed3
FLORIANE BERTHIER <floriane.berthier96@gmail.com>
2023-03-30 11:43:46 +0200

    add props.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import PokemonCard from "./components/PokemonCard";
 
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function App() {
+  const pokemon = pokemonList[0];
+
   return (
     <div>
-      <PokemonCard />
+      <PokemonCard pokemon={pokemon} />
     </div>
   );
 }
-export default PokemonCard;
+export default App;
